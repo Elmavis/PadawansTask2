edbeaedce2155507ca9bb3f3db08734536814d5a
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-14 09:13:06 +0300

    PlusOverflow

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -9,15 +9,15 @@ namespace PadawansTask2
             if (count <= 0)
                 throw new ArgumentException();
             long sum = number;
-            for (int i = 1; i < count; i++)
+            for (long i = 1; i < count; i++)
             {
-                int newElem = number + i * add;
+                long newElem = (long)number + i * (long)add;
                 sum += newElem;
             }
-            if (sum > Int32.MaxValue)
+            if (sum > Int32.MaxValue || sum < Int32.MinValue)
                 throw new OverflowException();
 
-            return sum;
+            return (int)sum;
         }
     }
 }
